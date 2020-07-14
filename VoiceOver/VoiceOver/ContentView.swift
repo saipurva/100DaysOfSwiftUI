5fb3f4685b5c7a7bc38811e419bb6755309971e9
saipurva <64395082+saipurva@users.noreply.github.com>
2020-07-14 08:00:38 +0100

    Accesibility

---
+++ VoiceOver/VoiceOver/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  VoiceOver
+//
+//  Created by Diana Harjani on 08/06/2020.
+//  Copyright © 2020 Saipurva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+//    let pictures = [
+//    "ales-krivec-15949",
+//    "galina-n-189483",
+//    "kevin-horstmann-141705",
+//    "nicolas-tissot-335096"
+//    ]
+//    let labels = [
+//    "Tulips", "Frozen tree buds", "Fireworks"]
+//
+//    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    @State private var estimate = 0.25
+    @State private var rating = 3
+    
+    
+    var body: some View {
+//        Slider(value: $estimate, in: 0...50)
+//        .padding()
+//        .accessibility(value: Text("\(Int(estimate))"))
+        
+        Stepper("Rate our services \(rating) /5" ,value: $rating, in: 1...5)
+        .accessibility(value: Text("\(rating) out of 5"))
+        
+        
+        
+//        Image(decorative: "character")
+//        .accessibility(hidden: true)
+//        VStack{
+//            Text("Your score is")
+//            Text("1000")
+//                .font(.title)
+//        }.accessibilityElement(children: .ignore)
+//        .accessibility(label: Text("Your score is 1000"))
+        
+        
+//       Image(pictures[selectedPicture])
+//        .resizable()
+//        .scaledToFit()
+//        .accessibility(label: Text(labels[selectedPicture]))
+//        .accessibility(addTraits: .isButton)
+//        .accessibility(removeTraits: .isImage)
+//        .onTapGesture {
+//            self.selectedPicture = Int.random(in: 0...3)
+//        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
